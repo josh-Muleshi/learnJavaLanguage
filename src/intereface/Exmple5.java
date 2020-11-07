5135fc38b10f0dc08a8274b4139b2385fbe1f185
josh muleshi <jmuleshi2@gmail.com>
2020-11-07 14:12:55 +0000

    an anonnyme class which the lambda expression

---
+++ src/intereface/Exmple5.java
@@ -0,0 +1,45 @@
+package intereface;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Exmple5 extends JFrame {
+
+    private static final long serialVersionUID = -5488220951900494888L;
+
+    private JButton btnClickMe = new JButton("Click Me");
+    private JButton btnPushMe = new JButton("Click Me");
+    private JButton btnActivateMe = new JButton("Click Me");
+
+    public Exmple5() {
+        super("Implementation d'Interface");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout( new FlowLayout());
+
+        contentPane.add( btnClickMe);
+        contentPane.add(btnPushMe);
+        contentPane.add(btnActivateMe);
+
+        /*btnClickMe.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnClickMe.setText("btn");
+                btnClickMe.setForeground(Color.cyan);
+            }
+        });*/
+
+        //btnClickMe.addActionListener((e) -> System.out.println("ok"));
+
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Exmple5();
+    }
+}
