9b47e45064f4a14bb2164139b03e281349cb97a2
josh muleshi <jmuleshi2@gmail.com>
2020-11-07 10:43:20 +0000

    my first graphic interface implementation

---
+++ src/intereface/Exmple0.java
@@ -0,0 +1,31 @@
+package intereface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exmple0 extends JFrame {
+
+    private JButton btnClickMe = new JButton("Click Me");
+    private JButton btnPushMe = new JButton("Click Me");
+    private JButton btnActivateMe = new JButton("Click Me");
+
+    public Exmple0() {
+        super("Implementation d'Interface");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout( new FlowLayout());
+
+        contentPane.add( btnClickMe);
+        contentPane.add(btnPushMe);
+        contentPane.add(btnActivateMe);
+
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Exmple0();
+    }
+}
