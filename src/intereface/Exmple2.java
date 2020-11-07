0f2e4b4853caf268e58142be16122a7ab8c57af6
josh muleshi <jmuleshi2@gmail.com>
2020-11-07 11:45:44 +0000

    addition of an other file

---
+++ src/intereface/Exmple2.java
@@ -0,0 +1,33 @@
+package intereface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exmple2 extends JFrame {
+
+    private static final long serialVersionUID = -5488220951900494888L;
+
+    private JButton btnClickMe = new JButton("Click Me");
+    private JButton btnPushMe = new JButton("Click Me");
+    private JButton btnActivateMe = new JButton("Click Me");
+
+    public Exmple2() {
+        super("Implementation d'Interface");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout( new FlowLayout());
+
+        contentPane.add( btnClickMe);
+        contentPane.add(btnPushMe);
+        contentPane.add(btnActivateMe);
+
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Exmple2();
+    }
+}
